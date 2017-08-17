3a3230b2e1400f0275e37e069f000c0a92019d2c
kerthikeyan <31098118+kerthikeyan@users.noreply.github.com>
2017-08-17 16:54:40 +0530

    C Program
    To find the giving value is odd or even

---
+++ OddorEven.C
@@ -0,0 +1,13 @@
+#include<conio.h>
+#include<stdio.h>
+main()
+{
+int a;
+printf("enter the value:")
+scanf("%d")
+if(a/2)
+printf("Given number is Even")
+ifelse
+printf("Given number is Odd")
+getch()
+}
